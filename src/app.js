bd838d7c2ee005d2e1541368e25c8835fea49095
Amelia Sayle <amelia@co-asayle1910.local>
2020-10-26 17:53:51 -0600

    tenth

--- src/app.js
+++ src/app.js
@@ -1,30 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import IndecisionApp from './components/IndecisionApp'
+import './styles/styles.scss'
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
 
-class OldSyntax {
-    constructor() {
-        this.name = 'mike'
-        this.getGreeting = this.getGreeting.bind(this)
-    }
-    getGreeting() {
-        return `Hi. my name is ${this.name}.`
-    }
-}
-const oldSyntax = new OldSyntax()
-const getGreeting = oldSyntax.getGreeting
-console.log(getGreeting())
 
-// ---------new class properties syntax
 
-class NewSyntax {
-    name = 'jen'
-    getGreeting = () => {
-        return `Hi. my name is ${this.name}.`
-    }
-}
-const newSyntax = new NewSyntax()
-const newGetGreeting = newSyntax.getGreeting
-console.log(newGetGreeting())
+
+
+// class OldSyntax {
+//     constructor() {
+//         this.name = 'mike'
+//         this.getGreeting = this.getGreeting.bind(this)
+//     }
+//     getGreeting() {
+//         return `Hi. my name is ${this.name}.`
+//     }
+// }
+// const oldSyntax = new OldSyntax()
+// const getGreeting = oldSyntax.getGreeting
+// console.log(getGreeting())
+
+// // ---------new class properties syntax
+
+// class NewSyntax {
+//     name = 'jen'
+//     getGreeting = () => {
+//         return `Hi. my name is ${this.name}.`
+//     }
+// }
+// const newSyntax = new NewSyntax()
+// const newGetGreeting = newSyntax.getGreeting
+// console.log(newGetGreeting())
--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -3,18 +3,25 @@ import AddOption from './AddOption'
 import Header from './Header'
 import Action from './Action'
 import Options from './Options'
+import OptionModal from './OptionModal'
 
 export default class IndecisionApp extends React.Component {
     state = {
-         options: []
+         options: [],
+         selectedOption: undefined
     }
     handlePick = (e) => {
         const randomNum = Math.floor(Math.random() * this.state.options.length)
         const option = this.state.options[randomNum]
-        alert(option)
+        this.setState(() => ({ 
+            selectedOption: option
+        }))
     }
     handleDeleteOptions = (e) => {
         this.setState(() => ({ options: [] }))
+    }
+    handleClearSelectedOption = () => {
+        this.setState(() => ({ selectedOption: undefined }))
     }
     handleDeleteOption = (optionToRemove) => {
         this.setState((prevState) => ({
@@ -66,6 +73,10 @@ export default class IndecisionApp extends React.Component {
             />
           <AddOption 
             handleAddOption={this.handleAddOption}
+          />
+          <OptionModal 
+            selectedOption={this.state.selectedOption}
+            handleClearSelectedOption={this.handleClearSelectedOption}
           />
         </div>
       );
